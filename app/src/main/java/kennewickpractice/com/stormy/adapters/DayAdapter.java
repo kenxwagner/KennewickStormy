d331e287d1d47f8bf3a0c9b6657b00530b1153d0
Ken Wagner <ken_xwagner@hotmail.com>
2015-09-24 16:59:05 -0700

    Connecting interface with data supplied by the API sent as JSON data

---
+++ app/src/main/java/kennewickpractice/com/stormy/adapters/DayAdapter.java
@@ -0,0 +1,67 @@
+package kennewickpractice.com.stormy.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import kennewickpractice.com.stormy.R;
+import kennewickpractice.com.stormy.weather.Day;
+
+/**
+ * Created by ken.wagner on 9/24/2015.
+ */
+public class DayAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private Day[] mDays;
+
+    public DayAdapter(Context context, Day[] days) {
+        mContext = context;
+        mDays = days;
+    }
+
+    @Override
+    public int getCount() {
+        return mDays.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDays[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0; // Not being used. Allows easy reference by tagging item.
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            //new to the world
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.temperatureLabel);
+            holder.dayLabel = (TextView)convertView.findViewById(R.id.dayNameLabel);
+
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        return null;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView;
+        TextView temperatureLabel;
+        TextView dayLabel;
+    }
+}
--- app/src/main/java/kennewickpractice/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/kennewickpractice/com/stormy/ui/DailyForecastActivity.java
@@ -19,6 +19,7 @@ public class DailyForecastActivity extends ListActivity {
         setListAdapter(adapter);
 
 
+
     }
 
 }

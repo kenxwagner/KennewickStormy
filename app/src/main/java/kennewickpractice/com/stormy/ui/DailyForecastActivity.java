ee3af5045f686ed793e0952a67b96a334003d922
Ken Wagner <ken_xwagner@hotmail.com>
2015-09-24 13:06:42 -0700

    added array adapter

--- app/src/main/java/kennewickpractice/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/kennewickpractice/com/stormy/ui/DailyForecastActivity.java
@@ -1,21 +1,24 @@
 package kennewickpractice.com.stormy.ui;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 import kennewickpractice.com.stormy.R;
 
-public class DailyForecastActivity extends AppCompatActivity {
-
+public class DailyForecastActivity extends ListActivity {
+    String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-    }
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, daysOfTheWeek);
+        setListAdapter(adapter);
+
+
+    }
 
 }

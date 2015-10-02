1bf2589f5e9f8db9fa48a6ae28edfc3c80c8f51d
kenxwagner <ken_xwagner@hotmail.com>
2015-10-01 17:04:53 -0700

    Time to make items on "Weather this week" clickable.

--- app/src/main/java/kennewickpractice/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/kennewickpractice/com/stormy/ui/DailyForecastActivity.java
@@ -5,6 +5,9 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
@@ -28,5 +31,19 @@ public class DailyForecastActivity extends ListActivity {
 
         DayAdapter adapter = new DayAdapter(this, mDays);
         setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+        String conditions = mDays[position].getSummary();
+        String highTemp = mDays[position].getTemperatureMax() + "";
+        String message = String.format("On %s a high of %s is expected with %s condition",
+                dayOfTheWeek,
+                highTemp,
+                conditions);
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
